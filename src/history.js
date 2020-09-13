53d9b9488ff48e03801f444c1f7137a209ae8df9
Akshay Shahi <shahiakshay31@gmail.com>
2020-09-13 19:26:09 +0530

    Update history.js

--- src/history.js
+++ src/history.js
@@ -1,10 +1,9 @@
-const Table = require('cli-table3');
 const { hist } = require('./utils');
 
 module.exports = {
-    handler: async function getCountriesAffected(args) {
+    handler: async function getCountriesAffected() {
 
         const data = await hist();
-        console.log(data);
+        console.log(`This is the list of ${data.length} countries affected => ${data}`);
     }
 }
